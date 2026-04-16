4512009b405bbfbbdca11c5e311ed245ddd726b4
Le Vivilet <levivilet@pm.me>
2026-04-16 14:14:18 +0200

    simplify

--- packages/blob-worker/src/parts/SendMessagePortToExtensionHostWorker/SendMessagePortToExtensionHostWorker.ts
+++
@@ -1,5 +0,0 @@
-import { RendererWorker, RpcId } from '@lvce-editor/rpc-registry'
-
-export const sendMessagePortToblobProcess = async (port: MessagePort): Promise<void> => {
-  await RendererWorker.sendMessagePortToExtensionHostWorker(port, RpcId.blobWorker)
-}
--- packages/blob-worker/src/parts/SendMessagePortToFileSystemProcess/SendMessagePortToFileSystemProcess.ts
+++
@@ -1,6 +0,0 @@
-import { RendererWorker, RpcId } from '@lvce-editor/rpc-registry'
-
-export const sendMessagePortToblobProcess = async (port: MessagePort): Promise<void> => {
-  const command = 'HandleMessagePortForblobProcess.handleMessagePortForblobProcess'
-  await RendererWorker.invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToSharedProcess', port, command, RpcId.blobWorker)
-}
