745191ea4eadd98933d57ca0db7e4fb55738a570
Le Vivilet <levivilet@pm.me>
2026-04-16 14:13:05 +0200

    simplify

--- packages/blob-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/blob-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,8 +1,10 @@
 import { base64StringToBlob } from 'blob-util'
 import { binaryStringToBlob, blobToBinaryString } from '../Blob/Blob.ts'
+import { handleMessagePort } from '../HandleMessagePort/HandleMessagePort.ts'
 
 export const commandMap: Record<string, any> = {
   'Blob.base64StringToBlob': base64StringToBlob,
   'Blob.binaryStringToBlob': binaryStringToBlob,
   'Blob.blobToBinaryString': blobToBinaryString,
+  'Blob.handleMessagePort': handleMessagePort,
 }
