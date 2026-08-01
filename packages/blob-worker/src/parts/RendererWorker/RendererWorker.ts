dbfb0d94b82d2e7a0880fa7d49694e7aee16d707
Le Vivilet <levivilet@pm.me>
2026-08-01 11:13:50 -0700

    feature: update eslint dependencies (#16)

--- packages/blob-worker/src/parts/RendererWorker/RendererWorker.ts
+++ packages/blob-worker/src/parts/RendererWorker/RendererWorker.ts
@@ -1,3 +1,14 @@
+import type { Rpc } from '@lvce-editor/rpc'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
-export const { invokeAndTransfer, sendMessagePortToExtensionHostWorker, set } = RendererWorker
+export const invokeAndTransfer = (method: string, ...params: readonly unknown[]): Promise<unknown> => {
+  return RendererWorker.invokeAndTransfer(method, ...params)
+}
+
+export const sendMessagePortToExtensionHostWorker = (port: MessagePort, rpcId?: number): Promise<void> => {
+  return RendererWorker.sendMessagePortToExtensionHostWorker(port, rpcId)
+}
+
+export const set = (rpc: Rpc): void => {
+  RendererWorker.set(rpc)
+}
